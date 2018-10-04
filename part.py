80ba241dfc8fcbee38013fda47e18f4aababb506
cindyKAULITZ <tony320cindy@gmail.com>
2018-10-04 13:53:10 +0800

    fix the variable to fit all data file

--- part.py
+++ part.py
@@ -1,20 +1,28 @@
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 import numpy as np
 #read file
 ### testing successed expect 2Circles2.txt ###
-f = open('2ring.txt','r')
+f = open('2CS.txt','r')
 param = f.read()
 arr = [e[0:] for e in param.split('\n')]
 data = [' '] * len(arr)
-print(arr)
+output = [' ']*len(arr)
+x_range = [' ']*len(arr)
+#print(arr)
 for i in range(0,len(arr)):
     temp = [e[0:] for e in arr[i].split(' ')]
     data[i] = [' '] * len(temp)
     for j in range(0,len(temp)):
         if j == (len(temp)-1):
             data[i][j] = int(temp[j])
+            output[i] = int(temp[j])
         else:
             data[i][j] = float(temp[j])
+            x_range[i] = float(temp[j])
+
+c = np.unique(output)
+xr = np.unique(x_range)
 #print(data)
 
 #learning function
@@ -27,26 +35,26 @@ stop = 0
 precise = 5
 while stop < len(data) :
     #print(data[n%4][2])
-    sgn = round((w[0]*thr) + (w[1]*data[n%4][0]) + (w[2]*data[n%4][1]),precise)
+    sgn = round((w[0]*thr) + (w[1]*data[n%len(data)][0]) + (w[2]*data[n%len(data)][1]),precise)
     print('sgn = ',sgn)
     if sgn > 0:
-        y = 2 
+        y = c.max() 
         print('y=1')
     else:
-        y = 1
+        y = c.min()
         print('y=0')
 
-    if y > data[n%4][2]:
+    if y > data[n%len(data)][2]:
         print('y>')
         w[0] = round(w[0] - (learn*thr),precise)
-        w[1] = round(w[1] - (learn*data[n%4][0]),precise)
-        w[2] = round(w[2] - (learn*data[n%4][1]),precise)
+        w[1] = round(w[1] - (learn*data[n%len(data)][0]),precise)
+        w[2] = round(w[2] - (learn*data[n%len(data)][1]),precise)
         stop = 0
-    elif y < data[n%4][2]:
+    elif y < data[n%len(data)][2]:
         print('y<')
         w[0] = round(w[0] + (learn*thr),precise)
-        w[1] = round(w[1] + (learn*data[n%4][0]),precise)
-        w[2] = round(w[2] + (learn*data[n%4][1]),precise)
+        w[1] = round(w[1] + (learn*data[n%len(data)][0]),precise)
+        w[2] = round(w[2] + (learn*data[n%len(data)][1]),precise)
         stop = 0 
     else:
         print('y=')
@@ -57,13 +65,21 @@ while stop < len(data) :
     
 # make plot
 
-# X = np.random.normal(0, 1, 30) # 每一个点的X值
-# Y = np.random.normal(0, 1, 30) # 每一个点的Y值
-# T = np.arctan2(Y,X) # for color value
+x = np.linspace(xr.min(),xr.max(),1000) # 100 linearly spaced numbers
+y = ((-w[1])/w[2])*x + (w[0]/w[2])
 
-# plt.scatter(X, Y, s=75, c=T, alpha=.5)
-# plt.xlim(-1.5, 1.5)
-# plt.ylim(-1.5, 1.5)
+plt.xlim =(-100,100)
+plt.ylim = (-100,100) 
+T = ('salmon', 'royalblue', 'green') # for color value
+cmap = ListedColormap(T[:3])
+plt.plot(x,y)
+#plt.quiver(w[1], w[2], color=('salmon'))
+for index in range(0,len(data)):
+    plt.scatter(x = data[index][0], y = data[index][1], s=30, c=cmap(data[index][2]), alpha=1)
 
 
-#plt.show()
+#plt.xlim(-1.5, 1.5)
+#plt.ylim(-1.5, 1.5)
+
+
+plt.show()
